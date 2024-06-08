ef6096c24d9ebc1a466f73d5889cfbe5fc386391
MarcosStanquini <marcossnarquini@gmail.com>
2024-06-08 12:21:10 -0300

    start-project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,18 @@ import { BrowserModule, provideClientHydration } from '@angular/platform-browser
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TestComponent } from './test/test.component';
+import { FirstModuleModule } from './first-module/first-module.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FirstModuleModule,
   ],
   providers: [
     provideClientHydration()
